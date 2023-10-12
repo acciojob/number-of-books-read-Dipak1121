8200bbf8962556d0582a30b0dcb4db4600de7c39
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-10-12 12:34:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -16,8 +16,15 @@ const library = [
   },
 ];
 
-const numberOfBooksRead = () => {
+const numberOfBooksRead = (library) => {
   // write your code here
+	let count = 0;
+	for ( let i = 0; i < library.length; i++ ){
+		if (library[i]["readingStatus"] == true){
+			count++;
+		}
+	}
+	return count;
 };
 
 // Do not change the code below
